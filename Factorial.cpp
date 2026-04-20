e2d58f0dfbe2db835575c5f1c0241534f1472e36
Archit Chhirolya <architchhirolya@gmail.com>
2026-04-20 23:59:24 +0530

    Factorial

---
+++ Factorial.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{   
+  if(n>1)
+  {
+    return n*factorial(n-1);
+  }
+  else
+  return 1;
+}
+
+int main()
+{
+    int inp;
+    cin>>inp;
+    cout<<factorial(inp);
+}

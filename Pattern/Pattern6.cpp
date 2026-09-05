5e540aef27f4be333659fbf01eb81c506fd4a4ad
Archit Chhirolya <architchhirolya@gmail.com>
2026-09-05 23:35:33 +0530

    Pattern printing

---
+++ Pattern/Pattern6.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a value;";
+    cin>>n;
+
+    for(int i=n; i>=0; i--)
+    {
+        for(int j=1; j<=i; j++)
+        cout<<j;
+
+        printf("\n");
+    }
+}
